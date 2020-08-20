26550c6ffd3a37ac81dcbed84f64a4526343f516
DacPhuc <phuc.nguyendac@hcmut.edu.vn>
2020-08-20 22:48:37 +0700

    Rest convention

--- src/main/java/com/nguyendacphuc/project/controller/UserController.java
+++ src/main/java/com/nguyendacphuc/project/controller/UserController.java
@@ -1,27 +1,32 @@
 package com.nguyendacphuc.project.controller;
 
-import com.nguyendacphuc.project.domain.BankSuccess;
-import com.nguyendacphuc.project.repository.BankSuccessRepository;
+import com.nguyendacphuc.project.domain.User;
+import com.nguyendacphuc.project.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
-import java.util.List;
 
 @RestController
+@RequestMapping(value = "users")
 public class UserController {
 
     @Autowired
-    BankSuccessRepository bankSuccessRepository;
+    UserRepository userRepository;
 
-    @GetMapping("/hello")
-    public ResponseEntity<?> index(){
-        List<BankSuccess> bankSuccessList = bankSuccessRepository.findByBankCode("ZPSGCB");
-        HashMap<String, List<BankSuccess>> result = new HashMap<>();
-        result.put("result", bankSuccessList);
+    @GetMapping()
+    public ResponseEntity<?> getListOfUser(){
+        Pageable pageable = PageRequest.of(0, 20);
+        Page<User> listUsers = userRepository.findAll(pageable);
+        HashMap<String, Page<User>> result = new HashMap<>();
+        result.put("result", listUsers);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
