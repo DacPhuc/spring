5b5568999e88bd70144bd028baf38a946ed0b6b2
DacPhuc <phuc.nguyendac@hcmut.edu.vn>
2020-08-22 01:06:00 +0700

    Schedule in spring

---
+++ src/main/java/com/nguyendacphuc/project/service/ScheduleService.java
@@ -0,0 +1,25 @@
+package com.nguyendacphuc.project.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.format.DateTimeFormatter;
+
+@Service
+public class ScheduleService {
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+    @Scheduled(fixedRate = 3000)
+    public void scheduleTaskWithFixedRate() {
+        System.out.println("Running schedule fix rate");
+    }
+
+    // Will take 2 second after invocation finish there task
+    @Scheduled(fixedDelay = 2000)
+    public void scheduleTaskWithFixedDelay() {}
+
+    @Scheduled(cron = "0 * * * * ?")
+    public void scheduleTaskWithCronExpression() {
+        System.out.println("Running schedule cron");
+    }
+}
